145b663b96d4f09d5898e8ac5f89756d009d7f61
Samuel <samuel.leobon@gmail.com>
2023-12-20 12:16:13 +0100

    🎨: made Prettier work and added data to API

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,86 @@
-import React, {useEffect, useState} from 'react'
-import { Image, Text, Heading, HStack, Center, Spacer, StackDivider, VStack } from '@chakra-ui/react'
-import {Header} from './Header.js'
-
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  Text,
+  Heading,
+  HStack,
+  Center,
+  Spacer,
+  StackDivider,
+  VStack,
+  Box,
+} from "@chakra-ui/react";
+import { Header } from "./Header.js";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
 
-  useEffect( () => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
   const slanted1 = {
-    transform: 'rotate(65deg)',
-    backgroundColor: '#F2F2F2'
-  }
+    transform: "rotate(65deg)",
+    backgroundColor: "#F2F2F2",
+  };
 
   const slanted2 = {
-    transform: 'rotate(125deg)',
-    backgroundColor: '#F2F2F2'
-  }
+    transform: "rotate(125deg)",
+    backgroundColor: "#F2F2F2",
+  };
   return (
     <>
-      <Header/>
+      <Header />
       <Center>
-      <HStack position="absolute" top="200%">
-          <Image
-          boxSize='25em'
-          objectFit='cover'
-          src='/Sleobon_portrait_carre.jpg'
-          alt='Samuel Léobon'
-          borderRadius="50%"
-          />
-          <Spacer/>
-          <VStack align="left" divider={<StackDivider borderColor='gray.200' />}>
-            <Heading color="black" fontSize="7xl">Samuel Léobon</Heading>
-            <Text>Futur étudiant en Mastère <i>Expert en développement Logiciel, Mobile & IOT</i> (20 ans)<br/>
-              à la recherche d’une alternance sur deux ans à partir d’octobre 2024.<br/>
-              Adaptable, ordonné, dynamique, impliqué. </Text>
+        <HStack position="absolute" top="200%">
+          <Box borderWidth="5px" borderRadius="50%" borderColor="#CF3333">
+            <Image
+              boxSize="25em"
+              objectFit="cover"
+              src="/Sleobon_portrait_carre.jpg"
+              alt="Samuel Léobon"
+              borderRadius="50%"
+            />
+          </Box>
+
+          <Spacer />
+          <VStack
+            align="left"
+            divider={<StackDivider borderColor="gray.200" />}
+            ml="20px"
+          >
+            <Heading color="black" fontSize="7xl">
+              Samuel Léobon
+            </Heading>
+            <Text>
+              Futur étudiant en Mastère{" "}
+              <i>Expert en développement Logiciel, Mobile & IOT</i> (20 ans)
+              <br />
+              à la recherche d’une alternance sur deux ans à partir d’octobre
+              2024.
+              <br />
+              Adaptable, ordonné, dynamique, impliqué.{" "}
+            </Text>
           </VStack>
-          
-      </HStack>
-      </Center>   
+        </HStack>
+      </Center>
       <div>
-        {(typeof backendData.users === 'undefined') ? (
+        {typeof backendData.users === "undefined" ? (
           <p>Loading API...</p>
         ) : (
           backendData.users.map((user, i) => {
-            <p key={i}>{user}</p>
+            <p key={i}>{user}</p>;
           })
         )}
       </div>
       <div style={slanted1}></div>
       <div style={slanted2}></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,65 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["user1", "user2", "user3"]});
-})
+const infos = {
+  formations: [
+    {
+      name: "Licence Informatique",
+      dates: "2021-2024",
+      description: "L1 Mathématiques-Informatique puis L2-L3 Informatique",
+    },
+    {
+      name: "Baccalauréat Général",
+      dates: "2021",
+      description:
+        "Spécialités Mathématiques / Numérique et Sciences Informatiques - Mention Bien",
+    },
+    {
+      name: "Certification TOEIC",
+      dates: "Janvier 2021",
+      description: "Score de 980/990 - Niveau C1",
+    },
+  ],
+  experience: [
+    {
+      company: "Angers Loire Habitat",
+      jobTitle: "Chargé d’accueil et secrétariat",
+      dates: "2 au 25 août 2023",
+    },
+    {
+      company: "Verisure",
+      jobTitle: "Conseiller Technique et Relation Client",
+      dates: "13 juin au 19 août 2022",
+      description:
+        "- Prise en charge et résolution des besoins client\n- Orientation du client vers les services adéquats\n- Manipulations administratives relatives aux besoins client",
+    },
+  ],
+  skills: {
+    POO: ["C++", "C#", "Java"],
+    Web: ["HTML/CSS", "JavaScript", "PHP", "Astro"],
+    BDD: ["SQL", "PostgreSQL"],
+    Autres: ["Python", "GitHub"],
+  },
+  interests: [
+    {
+      name: "Développement de jeux vidéos en amateur",
+    },
+    {
+      name: "Jeu de rôle sur table en association",
+      context: "Héritiers de Dumnacus",
+    },
+    {
+      name: "Worldbuilding",
+      context: "projet communautaire The World Beyond The Ice Wall",
+    },
+  ],
+  languages: ["Français natif", "Anglais C2", "Espagnol A2"],
+};
 
-app.listen(5000, () => { console.log("Server started on port 5000.") })
+app.get("/api", (req, res) => {
+  res.json(infos);
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000.");
+});
